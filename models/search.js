86001d64c65d0b15594b5cc57deee8152d0b0014
Noe Cuyuch <noe.cuyuch20@gmail.com>
2021-11-11 17:36:34 -0600

    Add custom city weather search

--- models/search.js
+++ models/search.js
@@ -7,12 +7,26 @@ class Search {
         //TODO: read DB if it exists
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1Ijoibm9lY3V5dWNoIiwiYSI6ImNrdnZkam42ZzAzMGoyb3FjenI2NjVmdDkifQ.mtMsAaodVg3Banfta6Pl2Q',
+            'limit': 5,
+            'language': 'en'
+        }
+    }
+
     //this method returns coincidences of a given city name
     async searchCity(city = '') {
 
         try {
 
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Madrid.json?language=en&access_token=pk.eyJ1Ijoibm9lY3V5dWNoIiwiYSI6ImNrdnZkam42ZzAzMGoyb3FjenI2NjVmdDkifQ.mtMsAaodVg3Banfta6Pl2Q&limit=5');
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json`,
+                params: this.paramsMapbox
+            });
+
+            const resp = await instance.get();
+
             console.log(resp.data);
 
         } catch (error) {
